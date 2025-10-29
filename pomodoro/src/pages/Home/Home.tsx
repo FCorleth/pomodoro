41cb09692fd6eb334d7382b0802673bf62dc7c50
FCorleth <zcorleth2005@gmail.com>
2025-10-29 08:26:33 -0300

    validations

--- pomodoro/src/pages/Home/Home.tsx
+++ pomodoro/src/pages/Home/Home.tsx
@@ -118,6 +118,7 @@ export function Home() {
 
     setCycles((cycles) => [...cycles, newCycle]);
     setActiveCycle(newCycle.id);
+    setAmountSecondsPassed(0);
 
     reset();
   }
@@ -125,13 +126,19 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   useEffect(() => {
+    let interval: number;
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate)
         );
       }, 1000);
     }
+
+    return () => {
+      clearInterval(interval);
+    };
   }, [activeCycle]);
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
@@ -140,6 +147,12 @@ export function Home() {
   const secondsAmount = currentSeconds % 60;
   const minutes = String(minutesAmount).padStart(2, "0");
   const seconds = String(secondsAmount).padStart(2, "0");
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`;
+    }
+  }, [minutes, seconds, activeCycle]);
 
   const { errors } = formState;
 
